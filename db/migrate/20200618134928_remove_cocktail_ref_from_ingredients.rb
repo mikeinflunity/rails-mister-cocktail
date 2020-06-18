1a24f43a895dc5de7542b204f6826b565628e608
michal szymkat <mikeszymkat@gmail.com>
2020-06-18 16:18:58 +0100

    coctails controller done

---
+++ db/migrate/20200618134928_remove_cocktail_ref_from_ingredients.rb
@@ -0,0 +1,5 @@
+class RemoveCocktailRefFromIngredients < ActiveRecord::Migration[6.0]
+  def change
+    remove_reference :ingredients, :cocktail, null: false, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Ingredient.create(name: "lemon")
+ Ingredient.create(name: "ice")
+ Ingredient.create(name: "mint leaves")
+
+ Cocktail.create(name: "Mojito")
+ Cocktail.create(name: "Caipirinha")
+ Cocktail.create(name: "Cuba Libre")
+
+Dose.create!(description: "1")
+Dose.create!(description: "2")
+Dose.create!(description: "20cl")
